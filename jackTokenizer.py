7141152df0dfa5f4483285da8c9de5b3d430e1ec
Winry <winry.tian@gmail.com>
2022-10-04 16:59:21 -0700

    add a line to strip self.current_token in JackTokenizer.advance()

--- jackTokenizer.py
+++ jackTokenizer.py
@@ -130,7 +130,7 @@ class JackTokenizer:
         # the maximum character index for this line. then do the same for the
         # line index. after finding the result, reverse it.
         return not (self.current_char_index == len(
-            self.stripped_lines[self.current_line_index])-1
+            self.stripped_lines[self.current_line_index]) - 1
                     and self.current_line_index == len(self.stripped_lines) - 1)
 
     # advances the current token to the next token
@@ -152,17 +152,25 @@ class JackTokenizer:
                 and self.current_line_index < len(self.stripped_lines) - 1):
             self.current_line_index += 1
             self.current_char_index = 0
-            self.current_char = self.stripped_lines[self.current_line_index][self.current_char_index]
+            self.current_char = self.stripped_lines[self.current_line_index][
+                self.current_char_index]
             return
 
         # for each character in the current line, starting from the current
         # character index, check if the character at curr_line[char_index]
         # is a delimiter. if it is, make current_char_index the char_index.
-        for char_index in range(self.current_char_index+1, len(curr_line)):
+        for char_index in range(self.current_char_index + 1, len(curr_line)):
             char = curr_line[char_index]
 
-            if self.is_delimiter(char) or char_index == len(curr_line)-1:
+            if self.is_delimiter(char) or char_index == len(curr_line) - 1:
+                # make the current token a substring of the current line from
+                # the current character index to char_index
                 self.current_token = curr_line[self.current_char_index:char_index]
+
+                # then strip the current token of whitespace. There should be
+                # no newlines in the current token because we already removed
+                # them.
+                self.current_token = self.current_token.strip(" ")
 
                 self.current_char_index = char_index
                 break
@@ -171,21 +179,23 @@ class JackTokenizer:
         delimiter_list = [0]
 
         # appends the index of every delimiter found to delimiter_list
-        for char_index in range(len(self.stripped_lines[self.current_line_index])):
+        for char_index in range(
+                len(self.stripped_lines[self.current_line_index])):
             char = self.stripped_lines[self.current_line_index][char_index]
             if self.is_delimiter(char):
                 delimiter_list.append(char_index)
                 if self.is_symbol(char):
-                    delimiter_list.append(char_index+1)
+                    delimiter_list.append(char_index + 1)
 
         # a list of all the slices I can make out of my delimiter list
         slice_list = []
 
         # print a list of slices of the current line's tokens
         for index in range(len(delimiter_list) - 1):
-            next_index = index+1
+            next_index = index + 1
 
-            curr_slice = curr_line[delimiter_list[index]:delimiter_list[next_index]]
+            curr_slice = curr_line[
+                         delimiter_list[index]:delimiter_list[next_index]]
             stripped_slice = curr_slice.strip(" ").strip("\n").strip(" ")
 
             if stripped_slice != "":
