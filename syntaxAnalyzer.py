9b239e583e5c791bf6b1b5186420ad109bb56097
Winry <winry.tian@gmail.com>
2022-10-07 10:08:33 -0700

    add new test.xml entries for symbols, keywords, ints, strings, and identifiers

--- syntaxAnalyzer.py
+++ syntaxAnalyzer.py
@@ -4,6 +4,9 @@ from TokenType import *
 # add a jackTokenizer
 jack_tokenizer = JackTokenizer()
 
+file = open("test.xml", "w")
+file.write("<token>\n")
+
 # while there are still more tokens, print out the tokenizer's current character
 # and advance the current letter.
 while jack_tokenizer.has_more_tokens():
@@ -19,24 +22,27 @@ while jack_tokenizer.has_more_tokens():
     match token_type:
         case TokenType.STRING_CONST:
             print(jack_tokenizer.string_val())
+            file.write(f"  <string>{jack_tokenizer.current_token}</string>\n")
 
         case TokenType.INT_CONST:
             print(jack_tokenizer.int_val())
+            file.write(f"  <int>{jack_tokenizer.current_token}</int>\n")
 
         case TokenType.SYMBOL:
             print(jack_tokenizer.symbol())
+            file.write(f"  <symbol>{jack_tokenizer.current_token}</symbol>\n")
 
         case TokenType.KEYWORD:
             print(jack_tokenizer.key_word())
+            file.write(f"  <keyword>{jack_tokenizer.current_token}</keyword>\n")
 
         case TokenType.IDENTIFIER:
             print(jack_tokenizer.identifier())
+            file.write(f"  <identifier>{jack_tokenizer.current_token}</identifier>\n")
 
         case other:
             print("not a token")
 
     print("\n")
 
-
-file = open("test.xml", "w")
-file.write("<tokens>hello!</tokens>")
+file.write("</token>")
